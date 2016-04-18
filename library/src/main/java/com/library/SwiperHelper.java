c575f509d007af410e711bcfa3adc587cd6a4cfc
supermanmwg <554607289@qq.com>
2016-04-19 07:23:13 +0800

    use MotionEvent.getRawX()
    getRawX() and getRawY() that is guaranteed to return absolute coordinates, relative to the device screen.
    
    While getX() and getY(), should return you coordinates, relative to the View, that dispatched them.

--- library/src/main/java/com/library/SwiperHelper.java
+++ library/src/main/java/com/library/SwiperHelper.java
@@ -29,8 +29,8 @@ public class SwiperHelper implements View.OnTouchListener {
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         int action = event.getAction();
-        int x = (int) event.getX();
-        int y = (int) event.getY();
+        int x = (int) event.getRawX();
+        int y = (int) event.getRawY();
 
         switch (action) {
             case MotionEvent.ACTION_DOWN:
@@ -39,8 +39,8 @@ public class SwiperHelper implements View.OnTouchListener {
                 mDownY = y;
                 break;
             case MotionEvent.ACTION_MOVE:
-                int dx = x - mDownX;
-                int dy = y - mDownY;
+                int dx = x - mLastX;
+                int dy = y - mLastY;
 
                 Log.d(TAG, "++++++++++++++");
                 Log.d(TAG, "X is " + mObserverView.getX() + ", Y is " + mObserverView.getY());
@@ -51,13 +51,20 @@ public class SwiperHelper implements View.OnTouchListener {
                 Log.d(TAG, "new X is " + newX + ", new y is " + newY);
                 Log.d(TAG, "move x is " + x + ", move y is " + y);
                 Log.d(TAG, "---------------");
+
                 mObserverView.setX(newX);
                 mObserverView.setY(newY);
 
+
+        /*        dx = x - mLastX;
+                dy = y - mLastY;
+                int scrollX = -dx;
+                int scrollY = -dy;
+                mObserverView.scrollBy(scrollX, scrollY);*/
                 break;
             case MotionEvent.ACTION_UP:
                 v.getParent().requestDisallowInterceptTouchEvent(false);
-                int observerViewX = (int) mObserverView.getX() + mObserverView.getWidth() / 2;
+              /*  int observerViewX = (int) mObserverView.getX() + mObserverView.getWidth() / 2;
                 int intervals = mSwipePicture.getWidth() / 3;
                 if (observerViewX < intervals) {
                     swipeLeft();
@@ -66,7 +73,18 @@ public class SwiperHelper implements View.OnTouchListener {
                 } else {
                     resetView();
                 }
-                break;
+                break;*/
+
+                int moveDistanceX = x - mDownX;
+                if(Math.abs(moveDistanceX) > 200) {
+                    if(moveDistanceX > 0) {
+                        swipeRight();
+                    } else {
+                        swipeLeft();
+                    }
+                } else {
+                    resetView();
+                }
         }
 
         mLastX = x;
